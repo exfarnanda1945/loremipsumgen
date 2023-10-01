d11596f51c70d08dc5e97de7e947cf068c27383d
exfarnanda1945 <exfarnanda1945@gmail.com>
2023-10-01 19:44:39 +0700

    add: Bottom sheet content generator result

---
+++ app/src/main/java/com/exfarnanda1945/loremipsumgen/presentation/components/BottomSheetContent.kt
@@ -0,0 +1,109 @@
+package com.exfarnanda1945.loremipsumgen.presentation.components
+
+import android.content.Context
+import android.widget.Toast
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.BottomSheetScaffoldState
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.unit.dp
+import com.exfarnanda1945.loremipsumgen.data.Resource
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BottomSheetContent(
+    result: Resource<String>,
+    context: Context,
+    scrollState: ScrollState,
+    bmState: BottomSheetScaffoldState,
+    scope: CoroutineScope
+) {
+    Column(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxSize()
+    ) {
+        Box(modifier = Modifier.weight(1f)) {
+            when (result) {
+                is Resource.Error -> Toast.makeText(
+                    context,
+                    result.message,
+                    Toast.LENGTH_SHORT
+                ).show()
+
+                is Resource.Loading -> Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    CircularProgressIndicator()
+                }
+
+                is Resource.Success -> Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(8.dp)
+                        .verticalScroll(scrollState)
+                ) {
+                    Text(result.data!!)
+                }
+
+            }
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 8.dp, vertical = 8.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            ElevatedButton(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = { /*TODO*/ },
+                colors = ButtonDefaults.elevatedButtonColors(
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    contentColor = MaterialTheme.colorScheme.onPrimary
+                ),
+            ) {
+                Icon(
+                    painter = rememberVectorPainter(image = Icons.Filled.Share),
+                    contentDescription = "Share"
+                )
+                Text("Share Result")
+            }
+            OutlinedButton(
+                onClick = {
+                    scope.launch {
+                        bmState.bottomSheetState.hide()
+                    }
+                },
+                modifier = Modifier
+                    .fillMaxWidth(),
+                border = BorderStroke(1.dp, MaterialTheme.colorScheme.primary)
+            ) {
+                Text("Close")
+            }
+        }
+
+    }
+}

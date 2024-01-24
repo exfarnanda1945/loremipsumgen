28ec7d71ba1597fca5e887c7ca3bc214a446389f
exfarnanda1945 <exfarnanda1945@gmail.com>
2024-01-24 22:17:27 +0700

    fix: change createdAt to Long

--- app/src/main/java/com/exfarnanda1945/loremipsumgen/feat_history/domain/models/HistoryGenerator.kt
+++ app/src/main/java/com/exfarnanda1945/loremipsumgen/feat_history/domain/models/HistoryGenerator.kt
@@ -1,8 +1,11 @@
 package com.exfarnanda1945.loremipsumgen.feat_history.domain.models
 
+
+
+
 data class HistoryGenerator(
     val id: Int,
-    val createdAt: String,
+    val createdAt: Long,
     val result: String,
     val setting: GeneratorSetting
 )

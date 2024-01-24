dfb791568c7418fbaa549bcacfcc66f8c68497bd
miko <exfar@samir.co.id>
2024-01-24 18:01:06 +0700

    add: generator db

---
+++ app/src/main/java/com/exfarnanda1945/loremipsumgen/core/database/GeneratorDatabase.kt
@@ -0,0 +1,18 @@
+package com.exfarnanda1945.loremipsumgen.core.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.exfarnanda1945.loremipsumgen.feat_history.data.dao.IGeneratorSettingDao
+import com.exfarnanda1945.loremipsumgen.feat_history.data.dao.IHistoryGeneratorDao
+import com.exfarnanda1945.loremipsumgen.feat_history.data.entities.GeneratorSettingEntity
+import com.exfarnanda1945.loremipsumgen.feat_history.data.entities.HistoryGeneratorEntity
+
+@Database(
+    entities = [GeneratorSettingEntity::class, HistoryGeneratorEntity::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class GeneratorDatabase : RoomDatabase() {
+    abstract val settingDao: IGeneratorSettingDao
+    abstract val historyDao: IHistoryGeneratorDao
+}

9ee46739f8e76d9622224d0431013c768ced02c6
miko <exfar@samir.co.id>
2024-01-24 18:02:55 +0700

    add: history repo & impl

---
+++ app/src/main/java/com/exfarnanda1945/loremipsumgen/feat_history/data/repository/HistoryGeneratorRepositoryImpl.kt
@@ -0,0 +1,29 @@
+package com.exfarnanda1945.loremipsumgen.feat_history.data.repository
+
+import com.exfarnanda1945.loremipsumgen.core.utils.Resource
+import com.exfarnanda1945.loremipsumgen.feat_history.data.dao.IGeneratorSettingDao
+import com.exfarnanda1945.loremipsumgen.feat_history.data.dao.IHistoryGeneratorDao
+import com.exfarnanda1945.loremipsumgen.feat_history.domain.models.HistoryGenerator
+import com.exfarnanda1945.loremipsumgen.feat_history.domain.repository.IHistoryRepository
+import javax.inject.Inject
+
+class HistoryGeneratorRepositoryImpl @Inject constructor(
+    private val settingDao: IGeneratorSettingDao,
+    private val historyDao: IHistoryGeneratorDao
+) : IHistoryRepository {
+    override suspend fun insert(data: HistoryGenerator): Resource<Unit> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun lists(): Resource<List<HistoryGenerator>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun get(id: Int): Resource<HistoryGenerator> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun delete(id: Int): Resource<Unit> {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/main/java/com/exfarnanda1945/loremipsumgen/feat_history/domain/repository/IHistoryRepository.kt
@@ -0,0 +1,11 @@
+package com.exfarnanda1945.loremipsumgen.feat_history.domain.repository
+
+import com.exfarnanda1945.loremipsumgen.core.utils.Resource
+import com.exfarnanda1945.loremipsumgen.feat_history.domain.models.HistoryGenerator
+
+interface IHistoryRepository {
+    suspend fun insert(data:HistoryGenerator):Resource<Unit>
+    suspend fun lists():Resource<List<HistoryGenerator>>
+    suspend fun get(id:Int):Resource<HistoryGenerator>
+    suspend fun delete(id:Int):Resource<Unit>
+}

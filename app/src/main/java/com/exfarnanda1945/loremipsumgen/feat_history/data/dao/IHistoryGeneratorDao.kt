e0c1654c222b0d955ecbd8fa2278f63441cc9687
miko <exfar@samir.co.id>
2024-01-26 16:25:33 +0700

    fix: history generator dao transaction

--- app/src/main/java/com/exfarnanda1945/loremipsumgen/feat_history/data/dao/IHistoryGeneratorDao.kt
+++ app/src/main/java/com/exfarnanda1945/loremipsumgen/feat_history/data/dao/IHistoryGeneratorDao.kt
@@ -4,16 +4,25 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Transaction
+import com.exfarnanda1945.loremipsumgen.feat_history.data.entities.HistoryAndSetting
 import com.exfarnanda1945.loremipsumgen.feat_history.data.entities.HistoryGeneratorEntity
 
 @Dao
 interface IHistoryGeneratorDao {
+    @Transaction
     @Insert
-    suspend fun create(history: HistoryGeneratorEntity)
+    suspend fun create(history: HistoryAndSetting):Long
 
+    @Transaction
     @Delete
     suspend fun delete(id: Int)
 
+    @Transaction
     @Query("SELECT * FROM history_generator WHERE id = :id")
-    fun get(id: Int): HistoryGeneratorEntity
+    fun get(id: Int): HistoryAndSetting
+
+    @Transaction
+    @Query("SELECT * FROM history_generator")
+    fun list(): List<HistoryAndSetting>
 }

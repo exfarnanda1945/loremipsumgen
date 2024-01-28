02fda76f6585aa236dcef8712cf1a2bd173010b4
exfarnanda1945 <exfarnanda1945@gmail.com>
2024-01-28 20:51:39 +0700

    add: generator screen wip

---
+++ app/src/main/java/com/exfarnanda1945/loremipsumgen/feat_generator/presentation/screen/generator/GeneratorScreen.kt
@@ -0,0 +1,27 @@
+package com.exfarnanda1945.loremipsumgen.feat_generator.presentation.screen.generator
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun GeneratorScreen() {
+    Scaffold {
+        Column(
+            modifier = Modifier
+                .padding(it)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Nah Bruh")
+        }
+    }
+}

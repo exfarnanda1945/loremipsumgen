19df7e9ed66732d9fe27e62ce4e50934be71453d
exfarnanda1945 <exfarnanda1945@gmail.com>
2024-01-28 20:49:11 +0700

    fix: move to ui core

--- app/src/main/java/com/exfarnanda1945/loremipsumgen/core/ui/theme/Theme.kt
+++ app/src/main/java/com/exfarnanda1945/loremipsumgen/core/ui/theme/Theme.kt
@@ -1,4 +1,4 @@
-package com.exfarnanda1945.loremipsumgen.ui.theme
+package com.exfarnanda1945.loremipsumgen.core.ui.theme
 
 import android.app.Activity
 import android.os.Build
--- app/src/main/java/com/exfarnanda1945/loremipsumgen/core/ui/theme/Type.kt
+++ app/src/main/java/com/exfarnanda1945/loremipsumgen/core/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.exfarnanda1945.loremipsumgen.ui.theme
+package com.exfarnanda1945.loremipsumgen.core.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle

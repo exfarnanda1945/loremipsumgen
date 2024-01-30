fe6de189bc18eb1bdfde29a84ddcfe7260bed907
miko <exfar@samir.co.id>
2024-01-30 17:35:25 +0700

    add: constant for db

--- app/src/main/java/com/exfarnanda1945/loremipsumgen/core/utils/Constant.kt
+++ app/src/main/java/com/exfarnanda1945/loremipsumgen/core/utils/Constant.kt
@@ -1,5 +1,15 @@
 package com.exfarnanda1945.loremipsumgen.core.utils
 
 object Constant {
+    /*
+    * Http
+    * */
     const val BASE_URL = "https://loripsum.net/api/"
+
+    /*
+    * Database
+    * */
+    const val DATABASE_NAME="generator"
+    const val SETTING_TABLE_NAME="generator_setting"
+    const val HISTORY_TABLE_NAME="history_generator"
 }

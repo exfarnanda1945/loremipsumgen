c23ffcdc6ea0f32b20dac5946c23eec795d35743
exfarnanda1945 <exfarnanda1945@gmail.com>
2024-01-30 07:32:05 +0700

    wip: generator screen

--- app/src/main/java/com/exfarnanda1945/loremipsumgen/feat_generator/presentation/screen/generator/GeneratorScreen.kt
+++ app/src/main/java/com/exfarnanda1945/loremipsumgen/feat_generator/presentation/screen/generator/GeneratorScreen.kt
@@ -1,27 +1,47 @@
 package com.exfarnanda1945.loremipsumgen.feat_generator.presentation.screen.generator
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun GeneratorScreen() {
+    val configuration = LocalConfiguration.current
+
     Scaffold {
         Column(
             modifier = Modifier
                 .padding(it)
                 .fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = "Nah Bruh")
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(configuration.screenHeightDp.dp / 3)
+                    .clip(RoundedCornerShape(bottomStart = 30.dp, bottomEnd = 30.dp))
+                    .background(
+                        Color.Black
+                    )
+            ) {
+
+            }
         }
     }
 }

0f8adc4d2bb1119114d1197a68f4dbd9515f66ec
miko <exfar@samir.co.id>
2024-01-30 17:22:35 +0700

    add: welcome screen

---
+++ app/src/main/java/com/exfarnanda1945/loremipsumgen/feat_generator/presentation/screen/welcome/WelcomeScreen.kt
@@ -0,0 +1,89 @@
+package com.exfarnanda1945.loremipsumgen.feat_generator.presentation.screen.welcome
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.exfarnanda1945.loremipsumgen.core.ui.theme.LoremipsumgenTheme
+
+@Composable
+fun WelcomeScreen() {
+    Scaffold {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Text(
+                    text = "loremipsum.gen", style = TextStyle(
+                        fontSize = 32.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                )
+                Spacer(modifier = Modifier.height(12.dp))
+                Text(
+                    text = "the LOREM IPSUM generator that doesn't suck", style = TextStyle(
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.SemiBold,
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                )
+                Spacer(modifier = Modifier.height(2.dp))
+                Text(
+                    text = "thanks for loripsum.net which has provide the API", style = TextStyle(
+                        fontSize = 14.sp,
+                        fontWeight = FontWeight.Normal,
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                )
+                Spacer(modifier = Modifier.height(30.dp))
+                ElevatedButton(onClick = { /*TODO*/ }, shape = RoundedCornerShape(6.dp)) {
+                    Text(
+                        text = "Lets generate the text",
+                        modifier = Modifier.padding(vertical = 8.dp, horizontal = 12.dp)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun WelcomeScreenPrev() {
+    LoremipsumgenTheme {
+        WelcomeScreen()
+    }
+}
+
+@Preview
+@Composable
+fun WelcomeScreenDarkPrev() {
+    LoremipsumgenTheme(darkTheme = true) {
+        WelcomeScreen()
+    }
+}

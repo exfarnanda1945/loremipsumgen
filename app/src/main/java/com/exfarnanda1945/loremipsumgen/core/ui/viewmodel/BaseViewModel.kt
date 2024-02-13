8b0c56fe162874730515a5b1c5218bf7579e386b
miko <exfar@samir.co.id>
2024-02-13 16:02:58 +0700

    add: BaseViewModel

---
+++ app/src/main/java/com/exfarnanda1945/loremipsumgen/core/ui/viewmodel/BaseViewModel.kt
@@ -0,0 +1,19 @@
+package com.exfarnanda1945.loremipsumgen.core.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.exfarnanda1945.loremipsumgen.core.ui.event.UiEvent
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+
+open class BaseViewModel :ViewModel() {
+    private var _mainChannel = Channel<UiEvent>()
+    val mainChannel = _mainChannel.receiveAsFlow()
+
+    protected fun sendUiEvent(uiEvent: UiEvent) {
+        viewModelScope.launch {
+            _mainChannel.send(uiEvent)
+        }
+    }
+}

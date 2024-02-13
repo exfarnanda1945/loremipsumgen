22703e527850fc3e6425842b92ab3f3b90b1effe
miko <exfar@samir.co.id>
2024-02-13 16:04:40 +0700

    fix: remove flow

--- app/src/main/java/com/exfarnanda1945/loremipsumgen/feat_generator/domain/usecase/GeneratorUseCase.kt
+++ app/src/main/java/com/exfarnanda1945/loremipsumgen/feat_generator/domain/usecase/GeneratorUseCase.kt
@@ -2,20 +2,14 @@ package com.exfarnanda1945.loremipsumgen.feat_generator.domain.usecase
 
 import com.exfarnanda1945.loremipsumgen.feat_generator.domain.repository.IGeneratorRepository
 import com.exfarnanda1945.loremipsumgen.core.utils.Resource
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 class GeneratorUseCase @Inject constructor(private val repository: IGeneratorRepository) {
-    suspend operator fun invoke(url: String): Flow<Resource<String>> = flow {
+    suspend operator fun invoke(url: String): Resource<String> {
         if (url.isBlank() || url.isEmpty()) {
-            emit(Resource.Failure("Url cannot be blank or empty"))
+            return Resource.Failure("Url cannot be blank or empty")
         }
 
-        val result = repository.generate(url)
-
-        emit(result)
-    }.flowOn(Dispatchers.IO)
+        return repository.generate(url)
+    }
 }

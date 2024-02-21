7381842dba1e899f357195f3c6d91e40a6780cea
miko <exfar@samir.co.id>
2024-02-21 15:40:46 +0700

    add: option expand state

---
+++ app/src/main/java/com/exfarnanda1945/loremipsumgen/feat_generator/presentation/screen/generator/OptionExpandState.kt
@@ -0,0 +1,6 @@
+package com.exfarnanda1945.loremipsumgen.feat_generator.presentation.screen.generator
+
+data class OptionExpandState(
+    val isMoreOptionExpand:Boolean=true,
+    val isHtmlOptionExpand:Boolean=true,
+)

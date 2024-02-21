bbf7417d7b06a5809d9ff6af46de0f4c0f9a688b
miko <exfar@samir.co.id>
2024-02-21 15:41:35 +0700

    fix: title style

--- app/src/main/java/com/exfarnanda1945/loremipsumgen/feat_generator/presentation/component/GeneratorOptions.kt
+++ app/src/main/java/com/exfarnanda1945/loremipsumgen/feat_generator/presentation/component/GeneratorOptions.kt
@@ -8,13 +8,14 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
 import androidx.compose.material.icons.filled.KeyboardArrowDown
-import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -32,23 +33,30 @@ fun GeneratorOptions(
             .padding(top = 14.dp)
     ) {
         Row(
-            modifier = Modifier.fillMaxWidth().clickable {
-                onExpandedClick()
-            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable {
+                    onExpandedClick()
+                },
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
             Text(
                 title,
-                style = MaterialTheme.typography.titleLarge.copy(fontSize = 16.sp),
+                style = TextStyle(
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.W500
+                ),
                 modifier = Modifier.padding(bottom = 4.dp)
             )
             Icon(
-                imageVector = if (isExpanded) Icons.Filled.KeyboardArrowDown else Icons.Filled.KeyboardArrowRight,
+                imageVector = if (isExpanded)
+                    Icons.Filled.KeyboardArrowDown else
+                    Icons.AutoMirrored.Filled.KeyboardArrowRight,
                 contentDescription = "",
                 modifier = Modifier
                     .padding(end = 20.dp)
 
-                )
+            )
         }
         AnimatedVisibility(
             visible = isExpanded,

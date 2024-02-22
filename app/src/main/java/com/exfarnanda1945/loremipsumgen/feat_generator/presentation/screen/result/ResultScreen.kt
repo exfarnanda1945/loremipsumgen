6cacb2d23969382d5ce1343f8baf5a070fe0f3fd
exfarnanda1945 <exfarnanda1945@gmail.com>
2024-02-22 21:13:29 +0700

    result screen topbar

--- app/src/main/java/com/exfarnanda1945/loremipsumgen/feat_generator/presentation/screen/result/ResultScreen.kt
+++ app/src/main/java/com/exfarnanda1945/loremipsumgen/feat_generator/presentation/screen/result/ResultScreen.kt
@@ -6,13 +6,23 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.exfarnanda1945.loremipsumgen.core.ui.component.HtmlParser
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ResultScreen(
     result: String,
@@ -20,7 +30,28 @@ fun ResultScreen(
     modifier: Modifier = Modifier
 ) {
     val scrollState = rememberScrollState()
-    Scaffold {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    titleContentColor = MaterialTheme.colorScheme.onPrimary,
+                ),
+                title = {
+                    Text("Result")
+                },
+                navigationIcon = {
+                    IconButton(onClick = { navHostController.popBackStack() }) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = "",
+                            tint = MaterialTheme.colorScheme.onPrimary
+                        )
+                    }
+                }
+            )
+        }
+    ) {
         Column(
             modifier = modifier
                 .padding(it)

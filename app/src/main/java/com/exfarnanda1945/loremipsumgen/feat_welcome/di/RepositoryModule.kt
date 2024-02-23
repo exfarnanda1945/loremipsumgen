5f17293ea10b693c2944d87cfcb4a416f3a3586d
miko <exfar@samir.co.id>
2024-02-23 13:58:58 +0700

    add: welcome repo module

---
+++ app/src/main/java/com/exfarnanda1945/loremipsumgen/feat_welcome/di/RepositoryModule.kt
@@ -0,0 +1,20 @@
+package com.exfarnanda1945.loremipsumgen.feat_welcome.di
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import com.exfarnanda1945.loremipsumgen.feat_welcome.data.repository.WelcomeRepositoryImpl
+import com.exfarnanda1945.loremipsumgen.feat_welcome.domain.repository.IWelcomeRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RepositoryModule {
+    @Provides
+    @Singleton
+    fun provideWelcomeRepository(ds: DataStore<Preferences>): IWelcomeRepository =
+        WelcomeRepositoryImpl(ds)
+}

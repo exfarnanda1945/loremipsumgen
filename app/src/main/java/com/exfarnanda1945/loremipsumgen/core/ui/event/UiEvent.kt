42b4b874ca486d1e1a19b3238c07acd34ff9c4c5
miko <exfar@samir.co.id>
2024-02-28 10:17:04 +0700

    fix: UiEvent navigate data nullable

--- app/src/main/java/com/exfarnanda1945/loremipsumgen/core/ui/event/UiEvent.kt
+++ app/src/main/java/com/exfarnanda1945/loremipsumgen/core/ui/event/UiEvent.kt
@@ -1,8 +1,8 @@
 package com.exfarnanda1945.loremipsumgen.core.ui.event
 
 sealed class UiEvent {
-    class ShowSnackBar(val msg:String):UiEvent()
-    class ShowToast(val msg:String):UiEvent()
-    class NavigateTo(val path:String):UiEvent()
-    class ShowLoading(val isLoading:Boolean) : UiEvent()
+    class ShowSnackBar(val msg: String) : UiEvent()
+    class ShowToast(val msg: String) : UiEvent()
+    class NavigateTo( val data: String? = null) : UiEvent()
+    class ShowLoading(val isLoading: Boolean) : UiEvent()
 }

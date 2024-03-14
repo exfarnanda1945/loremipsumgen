f1f47aedd509a53eefd871ce829a6150ad57e44c
miko <exfar@samir.co.id>
2024-03-14 09:42:43 +0700

    fix: passing path ui event navigate

--- app/src/main/java/com/exfarnanda1945/loremipsumgen/core/ui/event/UiEvent.kt
+++ app/src/main/java/com/exfarnanda1945/loremipsumgen/core/ui/event/UiEvent.kt
@@ -3,6 +3,6 @@ package com.exfarnanda1945.loremipsumgen.core.ui.event
 sealed class UiEvent {
     class ShowSnackBar(val msg: String) : UiEvent()
     class ShowToast(val msg: String) : UiEvent()
-    class NavigateTo( val data: String? = null) : UiEvent()
+    class NavigateTo(val path:String) : UiEvent()
     class ShowLoading(val isLoading: Boolean) : UiEvent()
 }

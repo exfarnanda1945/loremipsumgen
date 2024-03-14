f13c125b9b2f36766e333f1ded00e97a4688913b
miko <exfar@samir.co.id>
2024-03-14 09:37:02 +0700

    fix: rm datastore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -87,6 +87,4 @@ dependencies {
 
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
-
-    implementation("androidx.datastore:datastore-preferences:1.0.0")
 }

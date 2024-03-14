44641e886da6409fd94340b67b78136f3878ef5d
miko <exfar@samir.co.id>
2024-03-14 09:42:26 +0700

    fix: usecase module

--- app/src/main/java/com/exfarnanda1945/loremipsumgen/feat_result/di/ResultUseCaseModule.kt
+++ app/src/main/java/com/exfarnanda1945/loremipsumgen/feat_result/di/ResultUseCaseModule.kt
@@ -1,5 +1,18 @@
 package com.exfarnanda1945.loremipsumgen.feat_result.di
 
+import com.exfarnanda1945.loremipsumgen.feat_result.domain.repository.IResultRepository
+import com.exfarnanda1945.loremipsumgen.feat_result.domain.usecase.ResultUseCase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.scopes.ViewModelScoped
 
+@Module
+@InstallIn(ViewModelComponent::class)
 object ResultUseCaseModule {
+    @ViewModelScoped
+    @Provides
+    fun provideResultUseCase(repository: IResultRepository): ResultUseCase =
+        ResultUseCase(repository)
 }

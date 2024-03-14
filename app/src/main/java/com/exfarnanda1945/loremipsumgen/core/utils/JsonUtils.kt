035a29f4bddb9af45e083778be3df47f81e5ae52
miko <exfar@samir.co.id>
2024-03-14 09:41:48 +0700

    add: json utils

---
+++ app/src/main/java/com/exfarnanda1945/loremipsumgen/core/utils/JsonUtils.kt
@@ -0,0 +1,12 @@
+package com.exfarnanda1945.loremipsumgen.core.utils
+
+import com.google.gson.Gson
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class JsonUtils @Inject constructor(val gson: Gson) {
+    inline fun <reified T> toJson(data: T): String = gson.toJson(T::class.java)
+
+    inline fun <reified T> fromJson(value: String): T = gson.fromJson(value, T::class.java)
+}

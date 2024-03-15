603daafe673629f5f6819f6ae5d83d6120227c61
miko <exfar@samir.co.id>
2024-03-15 16:22:08 +0700

    feat: add snackbar wip

--- app/src/main/java/com/exfarnanda1945/loremipsumgen/feat_result/presentation/result/ResultScreen.kt
+++ app/src/main/java/com/exfarnanda1945/loremipsumgen/feat_result/presentation/result/ResultScreen.kt
@@ -1,26 +1,42 @@
 package com.exfarnanda1945.loremipsumgen.feat_result.presentation.result
 
- import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ContentCopy
+import androidx.compose.material.icons.filled.Save
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.NavHostController
 import com.exfarnanda1945.loremipsumgen.core.presentation.ui.component.HtmlParser
+import com.exfarnanda1945.loremipsumgen.core.presentation.ui.event.UiEvent
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -30,8 +46,39 @@ fun ResultScreen(
     navHostController: NavHostController,
     modifier: Modifier = Modifier
 ) {
+    val vm = hiltViewModel<ResultViewModel>()
+
     val scrollState = rememberScrollState()
+    val scope = rememberCoroutineScope()
+    val snackBarState = remember {
+        SnackbarHostState()
+    }
+
+    val lifecycle = LocalLifecycleOwner.current
+
+    LaunchedEffect(lifecycle) {
+        lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+            vm.mainChannel.collect {
+                when (it) {
+                    is UiEvent.ShowSnackBar -> scope.launch {
+                        snackBarState
+                            .showSnackbar(
+                                message = "Snackbar",
+                                actionLabel = "Action",
+                                duration = SnackbarDuration.Short
+                            )
+                    }
+
+                    else -> {}
+                }
+            }
+        }
+    }
+
     Scaffold(
+        snackbarHost = {
+            SnackbarHost(hostState = snackBarState)
+        },
         topBar = {
             TopAppBar(
                 colors = topAppBarColors(
@@ -49,9 +96,25 @@ fun ResultScreen(
                             tint = MaterialTheme.colorScheme.onPrimary
                         )
                     }
+                },
+                actions = {
+                    IconButton(onClick = { vm.onEvent(ResultEvent.OnCopyClipboard(result)) }) {
+                        Icon(
+                            imageVector = Icons.Filled.ContentCopy,
+                            contentDescription = "copy",
+                            tint = MaterialTheme.colorScheme.onPrimary
+                        )
+                    }
+                    Spacer(modifier = Modifier.width(14.dp))
+                    Icon(
+                        imageVector = Icons.Filled.Save,
+                        contentDescription = "save",
+                        tint = MaterialTheme.colorScheme.onPrimary
+                    )
+                    Spacer(modifier = Modifier.width(14.dp))
                 }
             )
-        }
+        },
     ) {
         Column(
             modifier = modifier

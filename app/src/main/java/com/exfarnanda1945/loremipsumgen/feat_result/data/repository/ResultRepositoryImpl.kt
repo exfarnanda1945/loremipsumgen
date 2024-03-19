781d825858c6d0df62e112920a004f5eeb9c944c
miko <exfar@samir.co.id>
2024-03-19 14:54:02 +0700

    fix: result repo impl

--- app/src/main/java/com/exfarnanda1945/loremipsumgen/feat_result/data/repository/ResultRepositoryImpl.kt
+++ app/src/main/java/com/exfarnanda1945/loremipsumgen/feat_result/data/repository/ResultRepositoryImpl.kt
@@ -1,8 +1,9 @@
 package com.exfarnanda1945.loremipsumgen.feat_result.data.repository
 
-import com.exfarnanda1945.loremipsumgen.core.data.database.entities.toEntity
+import com.exfarnanda1945.loremipsumgen.core.data.database.entities.HistoryGeneratorEntity
+import com.exfarnanda1945.loremipsumgen.core.domain.models.HistoryGenerator
+import com.exfarnanda1945.loremipsumgen.core.utils.JsonUtils
 import com.exfarnanda1945.loremipsumgen.core.utils.Resource
-import com.exfarnanda1945.loremipsumgen.feat_history.domain.models.HistoryGenerator
 import com.exfarnanda1945.loremipsumgen.feat_history.util.SafeCall
 import com.exfarnanda1945.loremipsumgen.feat_result.data.dao.IResultDao
 import com.exfarnanda1945.loremipsumgen.feat_result.domain.repository.IResultRepository
@@ -10,9 +11,20 @@ import dagger.hilt.android.scopes.ViewModelScoped
 import javax.inject.Inject
 
 @ViewModelScoped
-class ResultRepositoryImpl @Inject constructor(private val resultDao: IResultDao) :
+class ResultRepositoryImpl @Inject constructor(
+    private val resultDao: IResultDao,
+    private val jsonUtils: JsonUtils,
+) :
     IResultRepository {
+
     override suspend fun insert(data: HistoryGenerator): Resource<Long> = SafeCall {
-        resultDao.create(data.toEntity())
+        resultDao.create(
+            HistoryGeneratorEntity(
+                id = data.id,
+                result = data.result,
+                createdAt = data.createdAt,
+                setting = jsonUtils.toJson(data.setting)
+            )
+        )
     }
 }

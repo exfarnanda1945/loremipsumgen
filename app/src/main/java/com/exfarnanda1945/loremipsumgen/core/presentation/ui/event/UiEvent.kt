2c3d1aa748aa15ab5f87d5054e0aa308ab705409
miko <exfar@samir.co.id>
2024-03-19 14:16:49 +0700

    add: ui event navigate back

--- app/src/main/java/com/exfarnanda1945/loremipsumgen/core/presentation/ui/event/UiEvent.kt
+++ app/src/main/java/com/exfarnanda1945/loremipsumgen/core/presentation/ui/event/UiEvent.kt
@@ -3,6 +3,7 @@ package com.exfarnanda1945.loremipsumgen.core.presentation.ui.event
 sealed class UiEvent {
     class ShowSnackBar(val msg: String) : UiEvent()
     class ShowToast(val msg: String) : UiEvent()
-    class NavigateTo(val path:String) : UiEvent()
+    class NavigateTo(val path: String) : UiEvent()
+    object NavigateBack : UiEvent()
     class ShowLoading(val isLoading: Boolean) : UiEvent()
 }

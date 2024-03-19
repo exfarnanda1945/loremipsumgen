e9951572502536d0014192dfa9db0c94bc47d7d9
miko <exfar@samir.co.id>
2024-03-19 16:30:23 +0700

    fix: histoy repo impl

--- app/src/main/java/com/exfarnanda1945/loremipsumgen/feat_history/data/repository/HistoryGeneratorRepositoryImpl.kt
+++ app/src/main/java/com/exfarnanda1945/loremipsumgen/feat_history/data/repository/HistoryGeneratorRepositoryImpl.kt
@@ -1,24 +1,37 @@
 package com.exfarnanda1945.loremipsumgen.feat_history.data.repository
 
-import com.exfarnanda1945.loremipsumgen.core.data.database.entities.toModel
+import com.exfarnanda1945.loremipsumgen.core.domain.models.HistoryGenerator
+import com.exfarnanda1945.loremipsumgen.core.utils.JsonUtils
 import com.exfarnanda1945.loremipsumgen.core.utils.Resource
 import com.exfarnanda1945.loremipsumgen.feat_history.data.dao.IHistoryGeneratorDao
-import com.exfarnanda1945.loremipsumgen.feat_history.domain.models.HistoryGenerator
 import com.exfarnanda1945.loremipsumgen.feat_history.domain.repository.IHistoryRepository
 import com.exfarnanda1945.loremipsumgen.feat_history.util.SafeCall
 import javax.inject.Inject
 
 class HistoryGeneratorRepositoryImpl @Inject constructor(
-    private val historyDao: IHistoryGeneratorDao
+    private val historyDao: IHistoryGeneratorDao,
+    private val jsonUtils: JsonUtils
 ) : IHistoryRepository {
     override suspend fun lists(): Resource<List<HistoryGenerator>> = SafeCall {
         val list = historyDao.list()
-        list.map { it.toModel() }
+        list.map {
+            HistoryGenerator(
+                id = it.id,
+                createdAt = it.createdAt,
+                result = it.result,
+                setting = jsonUtils.fromJson(it.setting)
+            )
+        }
     }
 
-    override suspend fun get(id: Int): Resource<HistoryGenerator> = SafeCall{
+    override suspend fun get(id: Int): Resource<HistoryGenerator> = SafeCall {
         val get = historyDao.get(id)
-        get.toModel()
+        HistoryGenerator(
+            id = get.id,
+            createdAt = get.createdAt,
+            result = get.result,
+            setting = jsonUtils.fromJson(get.setting)
+        )
     }
 
     override suspend fun delete(id: Int): Resource<Unit> = SafeCall {

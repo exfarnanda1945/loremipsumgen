2ae915958d4116165c8bc87433dbcdc00c6303c2
miko <exfar@samir.co.id>
2024-03-19 14:51:29 +0700

    remove: generator setting entity

--- app/src/main/java/com/exfarnanda1945/loremipsumgen/core/data/database/GeneratorDatabase.kt
+++ app/src/main/java/com/exfarnanda1945/loremipsumgen/core/data/database/GeneratorDatabase.kt
@@ -2,13 +2,12 @@ package com.exfarnanda1945.loremipsumgen.core.data.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import com.exfarnanda1945.loremipsumgen.core.data.database.entities.GeneratorSettingEntity
 import com.exfarnanda1945.loremipsumgen.core.data.database.entities.HistoryGeneratorEntity
 import com.exfarnanda1945.loremipsumgen.feat_history.data.dao.IHistoryGeneratorDao
 import com.exfarnanda1945.loremipsumgen.feat_result.data.dao.IResultDao
 
 @Database(
-    entities = [GeneratorSettingEntity::class, HistoryGeneratorEntity::class],
+    entities = [ HistoryGeneratorEntity::class],
     version = 1,
     exportSchema = false
 )

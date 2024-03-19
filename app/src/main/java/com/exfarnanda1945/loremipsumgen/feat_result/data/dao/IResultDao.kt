1c9df548ac8e24dc91da3e6f0135ecdcc27b5cf9
miko <exfar@samir.co.id>
2024-03-19 14:53:38 +0700

    fix: move to core

--- app/src/main/java/com/exfarnanda1945/loremipsumgen/feat_result/data/dao/IResultDao.kt
+++ app/src/main/java/com/exfarnanda1945/loremipsumgen/feat_result/data/dao/IResultDao.kt
@@ -1,12 +1,14 @@
 package com.exfarnanda1945.loremipsumgen.feat_result.data.dao
 
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Transaction
-import com.exfarnanda1945.loremipsumgen.core.data.database.entities.HistoryAndSetting
+import com.exfarnanda1945.loremipsumgen.core.data.database.entities.HistoryGeneratorEntity
 
+@Dao
 interface IResultDao {
     @Transaction
     @Insert
-    suspend fun create(history: HistoryAndSetting): Long
+    suspend fun create(history: HistoryGeneratorEntity): Long
 
 }

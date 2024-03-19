08986d7ffc3aca170562a389c1023edf157e838f
miko <exfar@samir.co.id>
2024-03-19 14:48:05 +0700

    remove: onCopy success and failed

--- app/src/main/java/com/exfarnanda1945/loremipsumgen/core/utils/DeviceUtils.kt
+++ app/src/main/java/com/exfarnanda1945/loremipsumgen/core/utils/DeviceUtils.kt
@@ -3,23 +3,21 @@ package com.exfarnanda1945.loremipsumgen.core.utils
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
+import android.util.Log
 
 object DeviceUtils {
     fun copy(
         context: Context,
         data: String,
-        onCopySuccess: () -> Unit,
-        onCopyFailed: (msg: String?) -> Unit
     ) {
         val clipManager = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
         val clipData = ClipData.newPlainText("text", data)
 
         try {
             clipManager.setPrimaryClip(clipData)
-            onCopySuccess()
         } catch (e: Exception) {
             e.printStackTrace()
-            onCopyFailed(e.localizedMessage)
+            Log.e("DeviceUtils", "copy: ${e.localizedMessage}")
         }
     }
 }

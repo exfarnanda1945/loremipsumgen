7a343dbce1e4239a9658413fb18499d78474a3c8
miko <exfar@samir.co.id>
2024-03-19 14:56:03 +0700

    fix: generator state

--- app/src/main/java/com/exfarnanda1945/loremipsumgen/feat_generator/presentation/screen/generator/GeneratorState.kt
+++ app/src/main/java/com/exfarnanda1945/loremipsumgen/feat_generator/presentation/screen/generator/GeneratorState.kt
@@ -1,21 +1,20 @@
 package com.exfarnanda1945.loremipsumgen.feat_generator.presentation.screen.generator
 
-import com.exfarnanda1945.loremipsumgen.core.domain.model.GeneratorSetting
 import com.exfarnanda1945.loremipsumgen.feat_generator.utils.ParagraphLengthEnum
 
 data class GeneratorState(
-    override val numOfParagraphs: String = "1",
-    override val paragraphLength: ParagraphLengthEnum = ParagraphLengthEnum.SHORT,
-    override val isDecorate: Boolean = false,
-    override val isLink: Boolean = false,
-    override val isUl: Boolean = false,
-    override val isOl: Boolean = false,
-    override val isDl: Boolean = false,
-    override val isBq: Boolean = false,
-    override val isCode: Boolean = false,
-    override val isHeaders: Boolean = false,
-    override val isAllCaps: Boolean = false,
-    override val isPrudeVer: Boolean = false,
-    override val isReturnPlainText: Boolean = false,
-    override val isParagraphLengthShow: Boolean = false
-) : GeneratorSetting
+    val numOfParagraphs: String = "1",
+    val paragraphLength: ParagraphLengthEnum = ParagraphLengthEnum.SHORT,
+    val isDecorate: Boolean = false,
+    val isLink: Boolean = false,
+    val isUl: Boolean = false,
+    val isOl: Boolean = false,
+    val isDl: Boolean = false,
+    val isBq: Boolean = false,
+    val isCode: Boolean = false,
+    val isHeaders: Boolean = false,
+    val isAllCaps: Boolean = false,
+    val isPrudeVer: Boolean = false,
+    val isReturnPlainText: Boolean = false,
+    val isParagraphLengthShow: Boolean = false
+)

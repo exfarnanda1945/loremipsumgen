5e8238632334b6b980f176e14a9770b495fe7c64
miko <exfar@samir.co.id>
2024-03-19 14:53:19 +0700

    fix: delete query

--- app/src/main/java/com/exfarnanda1945/loremipsumgen/feat_history/data/dao/IHistoryGeneratorDao.kt
+++ app/src/main/java/com/exfarnanda1945/loremipsumgen/feat_history/data/dao/IHistoryGeneratorDao.kt
@@ -1,24 +1,23 @@
 package com.exfarnanda1945.loremipsumgen.feat_history.data.dao
 
 import androidx.room.Dao
-import androidx.room.Delete
 import androidx.room.Query
 import androidx.room.Transaction
-import com.exfarnanda1945.loremipsumgen.core.data.database.entities.HistoryAndSetting
+import com.exfarnanda1945.loremipsumgen.core.data.database.entities.HistoryGeneratorEntity
 import com.exfarnanda1945.loremipsumgen.core.utils.Constant.HISTORY_TABLE_NAME
 
 @Dao
 interface IHistoryGeneratorDao {
 
     @Transaction
-    @Delete
+    @Query("DELETE FROM $HISTORY_TABLE_NAME WHERE id = :id")
     suspend fun delete(id: Int)
 
     @Transaction
     @Query("SELECT * FROM $HISTORY_TABLE_NAME WHERE id = :id")
-    fun get(id: Int): HistoryAndSetting
+    fun get(id: Int): HistoryGeneratorEntity
 
     @Transaction
     @Query("SELECT * FROM $HISTORY_TABLE_NAME")
-    fun list(): List<HistoryAndSetting>
+    fun list(): List<HistoryGeneratorEntity>
 }

a0071a8a65b5af9f8200ad632cb47b80ce0b2360
miko <exfar@samir.co.id>
2024-03-19 14:50:58 +0700

    fix: move to core

--- app/src/main/java/com/exfarnanda1945/loremipsumgen/feat_history/domain/repository/IHistoryRepository.kt
+++ app/src/main/java/com/exfarnanda1945/loremipsumgen/feat_history/domain/repository/IHistoryRepository.kt
@@ -1,7 +1,7 @@
 package com.exfarnanda1945.loremipsumgen.feat_history.domain.repository
 
 import com.exfarnanda1945.loremipsumgen.core.utils.Resource
-import com.exfarnanda1945.loremipsumgen.feat_history.domain.models.HistoryGenerator
+import com.exfarnanda1945.loremipsumgen.core.domain.models.HistoryGenerator
 
 interface IHistoryRepository {
     suspend fun lists():Resource<List<HistoryGenerator>>
--- app/src/main/java/com/exfarnanda1945/loremipsumgen/feat_result/domain/usecase/ResultUseCase.kt
+++ app/src/main/java/com/exfarnanda1945/loremipsumgen/feat_result/domain/usecase/ResultUseCase.kt
@@ -1,7 +1,7 @@
 package com.exfarnanda1945.loremipsumgen.feat_result.domain.usecase
 
+import com.exfarnanda1945.loremipsumgen.core.domain.models.HistoryGenerator
 import com.exfarnanda1945.loremipsumgen.core.utils.Resource
-import com.exfarnanda1945.loremipsumgen.feat_history.domain.models.HistoryGenerator
 import com.exfarnanda1945.loremipsumgen.feat_result.domain.repository.IResultRepository
 import dagger.hilt.android.scopes.ViewModelScoped
 import javax.inject.Inject
